c00f6395bb05c23c1a7158c33b31ff0746ba9b8b
MichaelGamel <michael.gamell2012@gmail.com>
2017-07-20 12:57:46 +0200

    all app

---
+++ src/components/video_list.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = ({ videos, onVideoSelect }) => {
+
+    const videoItems = videos.map((video) => {
+        return <VideoListItem onVideoSelect={onVideoSelect} key={video.etag} video={video} />
+    })
+
+    return (
+        <ul className="col-md-4 list-group">
+            {/* {videos.length} */}
+            {videoItems}
+        </ul>
+    );
+}
+
+export default VideoList;
---
+++ src/components/video_list_item.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const VideoListItem = ({ video, onVideoSelect }) => {
+    //1- const video = props.video;
+    //2- const { video } = props;
+
+    const imageUrl = video.snippet.thumbnails.default.url;
+    return (
+        <li onClick={() => onVideoSelect(video)} className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img src={imageUrl} className="media-object" />
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">
+                        {video.snippet.title}
+                    </div>
+                </div>
+            </div>
+        </li>
+    )
+}
+
+export default VideoListItem;   
